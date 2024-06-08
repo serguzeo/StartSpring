87c3b99f90b2324d966bb7a6562ca4cfa6a8a89d
serguzeo <serguzeo@gmail.com>
2024-06-08 12:51:30 +0300

    -messages

--- src/main/java/com/serguzeo/StartSpring/repositories/IMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.serguzeo.StartSpring.repositories;
-
-import com.serguzeo.StartSpring.models.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface IMessageRepository extends JpaRepository<Message, UUID> {
-}
